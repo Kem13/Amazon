a80ab0125016ec1781bbd8fe3a5f77b550b54f82
Kem13 <mathew.kempke@yahoo.com>
2021-06-22 01:07:07 -0500

    header added

---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css';
+
+function Product() {
+    return (
+        <div className='product'>
+            <div className= 'product_info'>
+                <p>The lean startup</p>
+                <p className= 'product_price'>
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product_rating">
+                    <p></p>
+
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Product
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdU6SX66FdAB385EjIdHJRYj4bUinAb14",
+    authDomain: "fir-c1ccd.firebaseapp.com",
+    projectId: "fir-c1ccd",
+    storageBucket: "fir-c1ccd.appspot.com",
+    messagingSenderId: "265735591202",
+    appId: "1:265735591202:web:652c6562b0f1dfd66e817b"
+  };
